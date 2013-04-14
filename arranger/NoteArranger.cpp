11ca12e1fcd47aa596d58aa13cef88b9cb1570f4
acknak <acknak39@gmail.com>
2013-04-14 16:51:39 +0900

    change arrange method

--- arranger/NoteArranger.cpp
+++ arranger/NoteArranger.cpp
@@ -6,15 +6,11 @@ void NoteArranger::arrange(Score *score)
 {
   for (list<Note>::iterator it_notes=score->notes.begin(); it_notes!=score->notes.end(); ++it_notes) {
     vector<short> velocities;
-    if (it_notes->getVelocityPointNum()>0 || (velocities=it_notes->getVelocities()).size()==0)
+    if (it_notes->getVelocityPointNum() > 0)
       continue;
 
     unsigned short range = (velocities.size()-1) / 2;
     unsigned short ms_front_edge=nak::ms_front_edge, ms_back_edge=nak::ms_back_edge;
-    if (it_notes!=score->notes.begin() && boost::prior(it_notes)->getEnd()==it_notes->getStart() && it_notes->getOvrl()>0)
-      ms_front_edge = it_notes->getOvrl();
-    if (it_notes!=--score->notes.end() && boost::next(it_notes)->getStart()==it_notes->getEnd() && boost::next(it_notes)->getOvrl()>0)
-      ms_back_edge = boost::next(it_notes)->getOvrl();
 
     it_notes->addVelocityPoint(0, 0);
     it_notes->addVelocityPoint(ms_front_edge, it_notes->getBaseVelocity());
